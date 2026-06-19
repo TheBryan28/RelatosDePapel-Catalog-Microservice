094f7a7cc0538db0fdea8e07877fcb3f8bf50fa3
lkavila <kener1999avila@gmail.com>
2026-06-19 12:40:03 -0500

    optimize get supplies with predicate endpoint

--- src/main/java/com/relatosdepapel/catalog/repository/SupplyJpaRepository.java
+++ src/main/java/com/relatosdepapel/catalog/repository/SupplyJpaRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
 
@@ -48,4 +50,35 @@ public interface SupplyJpaRepository extends
     List<Supply> findByTitleContainingIgnoreCase(String title);
 
     List<SupplyBatchDto> findByIdInAndIsActiveTrue(List<BigInteger> ids);
+
+    @Query("""
+    SELECT DISTINCT s FROM Supply s
+    LEFT JOIN FETCH s.images
+    WHERE (:title IS NULL OR LOWER(s.title) LIKE LOWER(CONCAT('%', :title, '%')))
+    AND (:author IS NULL OR LOWER(s.author) LIKE LOWER(CONCAT('%', :author, '%')))
+    AND (:minPrice IS NULL OR s.price >= :minPrice)
+    AND (:maxPrice IS NULL OR s.price <= :maxPrice)
+    AND (:isActive IS NULL OR s.isActive = :isActive)
+""")
+    List<Supply> findSuppliesWithImages(
+            @Param("title") String title,
+            @Param("author") String author,
+            @Param("minPrice") BigDecimal minPrice,
+            @Param("maxPrice") BigDecimal maxPrice,
+            @Param("isActive") Boolean isActive
+    );
+
+    @Query("""
+        SELECT DISTINCT s FROM Supply s
+        LEFT JOIN FETCH s.images
+        WHERE s.id IN :ids
+    """)
+    List<Supply> findWithImagesByIds(@Param("ids") List<Integer> ids);
+
+    @Query("""
+        SELECT DISTINCT s FROM Supply s
+        LEFT JOIN FETCH s.categories
+        WHERE s.id IN :ids
+    """)
+    List<Supply> findWithCategoriesByIds(@Param("ids") List<Integer> ids);
 }
